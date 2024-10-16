2956997a00166d12e18b539280c7ecf871e37e3c
Hanif Fathurahman <58112881+HanifFathurahman@users.noreply.github.com>
2024-10-16 19:59:31 +0700

    Delete Semester 5/Tugas 4 directory

--- Semester 5/Tugas 4/angkatertinggi.java
+++
@@ -1,6 +0,0 @@
-public class angkatertinggi {
-    public static void main(String[] args) {
-        System.out.print("\f");
-        System.out.println("Angka yang tertinggi adalah: " + Math.max(9, 7));
-    }
-}
--- Semester 5/Tugas 4/bikin_object1.java
+++
@@ -1,9 +0,0 @@
-public class bikin_object1 {
-    int x;
-    
-    public static void main(String[] args) {
-        bikin_object1 object_ini = new bikin_object1();
-        object_ini.x = 40; System.out.println("\f");
-        System.out.println(object_ini.x);
-    }
-}
